54c9bcec1daabcbe176c8a2614ad4d19c0d9281c
Laura Rubio <laurarubio11@hotmail.com>
2019-02-03 19:11:00 -0500

    Modificaciones

--- ZonasWiFi/Program.cs
+++ ZonasWiFi/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -11,12 +12,15 @@ namespace ZonasWiFi
         /// <summary>
         /// Punto de entrada principal para la aplicación.
         /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
+       // [STAThread]
+       // static void Main()
+        //{
+          //  Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
+         //}
     }
 }
+
+
--- ZonasWiFi/Zona.cs
+++ ZonasWiFi/Zona.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,10 +20,52 @@ namespace ZonasWiFi
             this.nombre = nombre;
         }
 
-
+        public Zona()
+        {
+        }
 
         public double Local1 { get; set; }
         public double Local2 { get; set; }
         public string Nombre { get; set; }
+
+        public void readFiles()
+        {
+            StreamReader st = new StreamReader("...\\..\\archivos\\ZONAS_WIFI_GRATIS_PARA_LA_GENTE.csv");
+
+            string line = st.ReadLine();
+            line = st.ReadLine();
+            int conta = 0;
+            while (conta<303)
+            {
+
+                string[] info = line.Split(',');
+                string name = info[8];
+                string longitud = info[14];
+                string latitud = info[15];
+                writeFile(name, longitud, latitud);
+                conta++;
+                Console.WriteLine(conta + " CONTADOR");
+               
+                line = st.ReadLine();
+            }
+            st.Close();
+
+        }
+        public void writeFile(string name, string longitud, string latitud)
+        {
+            StreamWriter sw = new StreamWriter("...\\..\\archivos\\" + name + ".txt");
+            Console.WriteLine(name + " NOMBRE MUNICIPIO");
+           // Console.WriteLine(latitud + " LATITUD MUNICIPIO");
+            sw.WriteLine(longitud + "," + latitud);
+            sw.Close();
+        }
+        static void Main()
+        {
+            Zona z = new Zona();
+            z.readFiles();
+        }
+
+
+       
     }
 }
